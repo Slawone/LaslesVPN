7c4340420cb441bd843f6fe2d8449320c2c68b3c
Slawone <for1s8@ya.ru>
2024-02-02 14:44:09 +0300

    chage structure from js to html

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
-import App from './App.js';
+const body = document.body;
+const headerNav = document.querySelector('.header__nav');
+const themeBtn = document.querySelector('.header__theme');
+const themeSvg = document.querySelector('.header__theme-svg');
+const burgerBtn = document.querySelector('.header__burger');
 
-const API_BASE_URL = 'https://laslesvpn-4eab4-default-rtdb.europe-west1.firebasedatabase.app/.json';
-const $root = document.querySelector('#root');
+burgerBtn.addEventListener('click', () => {
+  burgerBtn.classList.toggle('active');
+  headerNav.classList.toggle('active');
+})
 
-fetch(API_BASE_URL)
-	.then((response) => response.json())
-	.then((data) => {
-		$root.insertAdjacentHTML('beforeend', App(data));
-	})
-	.catch((error) => console.error(error))
-
-// const $burgerBtn = document.querySelector('.header__burger');
-
-// $burgerBtn.addEventListener('click', () => {
-// 	$burgerBtn.classList.toggle('active');
-// });
+themeBtn.addEventListener('click', () => {
+  body.classList.toggle('dark');
+  themeSvg.classList.toggle('dark');
+})
